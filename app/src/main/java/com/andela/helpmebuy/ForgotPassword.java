f6578a8a811126c77b965fe2df4008e4e0d90fe5
princess-solo <jessicaprincess61@gmail.com>
2015-10-12 13:35:16 +0100

    Implement reset user password using firebase's resetPassword method

--- app/src/main/java/com/andela/helpmebuy/ForgotPassword.java
+++ app/src/main/java/com/andela/helpmebuy/ForgotPassword.java
@@ -1,14 +1,18 @@
 package com.andela.helpmebuy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -16,11 +20,11 @@ public class ForgotPassword extends AppCompatActivity {
 
     private Firebase firebase;
 
-    private RelativeLayout parentLayout;
+    private LinearLayout parentLayout;
 
     private EditText emailEditText;
 
-    private Button sendResetEmail;
+    private Button sendResetEmailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +32,17 @@ public class ForgotPassword extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);
         firebase = new Firebase(FIREBASE_URL);
+
         setContentView(R.layout.activity_forgot_password);
+
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
         actionBar.show();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        sendResetEmail = (Button) findViewById(R.id.send_reset_email_button);
+        sendResetEmailButton = (Button) findViewById(R.id.send_reset_email_button);
         emailEditText = (EditText) findViewById(R.id.send_reset_email_text);
+        parentLayout = (LinearLayout) findViewById(R.id.linear_layout);
     }
 
     @Override
@@ -45,6 +53,33 @@ public class ForgotPassword extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendTemporaryPassword(View view) {
+        final String email = emailEditText.getText().toString().trim();
+
+        if (email.equals("")) {
+            emailEditText.setError(getResources().getString(R.string.email_missing));
+        } else {
+            sendResetEmailButton.setEnabled(false);
+            final Intent intent = new Intent(this, SigninActivity.class);
+
+            firebase.resetPassword(emailEditText.getText().toString(), new Firebase.ResultHandler() {
+
+                @Override
+                public void onSuccess() {
+                    Snackbar.make(parentLayout, "Success", Snackbar.LENGTH_LONG).show();
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    Snackbar.make(parentLayout, "Failed", Snackbar.LENGTH_LONG).show();
+                    sendResetEmailButton.setEnabled(true);
+                }
+            });
+        }
     }
 
 }
