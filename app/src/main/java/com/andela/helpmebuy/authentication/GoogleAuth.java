92864a6b2622052e943f5214c66d910e1e87e136
princess-solo <jessicaprincess61@gmail.com>
2015-10-13 21:01:02 +0100

    Define AuthCallback interface and isolate google api into GoogleAuth class.

--- app/src/main/java/com/andela/helpmebuy/authentication/GoogleAuth.java
+++ app/src/main/java/com/andela/helpmebuy/authentication/GoogleAuth.java
@@ -1,4 +1,19 @@
 package com.andela.helpmebuy.authentication;
 
-public class GoogleAuth {
+import android.view.View;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class GoogleAuth implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onClick(View v) {
+        
+    }
+
+
 }
