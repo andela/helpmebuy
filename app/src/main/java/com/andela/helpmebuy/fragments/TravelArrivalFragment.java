e5cea5b59f9a72469ce285c92ced25bad287322d
andela-bomotoso <bukola.omotoso@andela.com>
2015-11-23 17:31:54 +0100

    Add user arrival details

---
+++ app/src/main/java/com/andela/helpmebuy/fragments/TravelArrivalFragment.java
@@ -0,0 +1,41 @@
+package com.andela.helpmebuy.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.andela.helpmebuy.R;
+
+public class TravelArrivalFragment extends Fragment {
+
+    private View location;
+
+    private TextView locationValue;
+
+    private View date;
+
+    private TextView dateValue;
+
+    private View time;
+
+    private TextView timeValue;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_arrival_infos,container,false);
+
+        location = view.findViewById(R.id.location);
+        locationValue = (TextView)view.findViewById(R.id.location_value);
+
+        date = view.findViewById(R.id.date);
+        dateValue = (TextView) view.findViewById(R.id.date_value);
+
+        time = view.findViewById(R.id.time);
+        timeValue = (TextView) view.findViewById(R.id.time_value);
+
+        return view;
+    }
+}
