bdd56e6836bc24b2ec3adc38b2fdbc572a69cf9a
andela-bomotoso <bukola.omotoso@andela.com>
2015-11-25 14:18:23 +0100

    Add a previous button on Arrival Details fragment

--- app/src/main/java/com/andela/helpmebuy/activities/CreateTravelActivity.java
+++ app/src/main/java/com/andela/helpmebuy/activities/CreateTravelActivity.java
@@ -4,6 +4,7 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.andela.helpmebuy.R;
 import com.andela.helpmebuy.dialogs.DatePickerFragment;
@@ -17,20 +18,21 @@ public class CreateTravelActivity extends AppCompatActivity {
 
     private TravelArrivalFragment travelArrivalFragment;
 
+    private LinearLayout parentLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_travel);
 
+        parentLayout = (LinearLayout)findViewById(R.id.departure_layout);
 
         travelDepartureFragment = new TravelDepartureFragment();
         travelArrivalFragment = new TravelArrivalFragment();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.create_travel_fragment_container, travelDepartureFragment)
-                .addToBackStack(null)
-                .commit();
+        displayDepartureDetails(parentLayout);
+
+
     }
 
     public void displayArrivalDetails(View view) {
@@ -39,6 +41,14 @@ public class CreateTravelActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.create_travel_fragment_container,travelArrivalFragment)
                 .addToBackStack(null).commit();
+    }
+
+    public void displayDepartureDetails(View view) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.create_travel_fragment_container, travelDepartureFragment)
+                .addToBackStack(null)
+                .commit();
     }
 
     public void closeWindow(View view) {
