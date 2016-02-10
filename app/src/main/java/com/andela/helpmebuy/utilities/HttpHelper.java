71c3b3ea003c2f3e9c588c4be35436221fd1fda0
Eston Karumbi <eston.karumbi@andela.com>
2016-02-10 15:03:24 +0300

    Created class HttpHelper

---
+++ app/src/main/java/com/andela/helpmebuy/utilities/HttpHelper.java
@@ -0,0 +1,42 @@
+package com.andela.helpmebuy.utilities;
+
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+public class HttpHelper implements JsonCallback {
+
+    private Context context;
+
+    public HttpHelper(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void getJson(String url, final HttpCallback httpCallback) {
+        Ion.with(context)
+                .load(url)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        if (e != null) {
+                            httpCallback.onError(e.getMessage());
+                        } else {
+                            //httpCallback.onSuccess(result.getAsString());
+                            String formattedAddress = result
+                                    .getAsJsonArray("results")
+                                    .get(0)
+                                    .getAsJsonObject()
+                                    .get("formatted_address")
+                                    .getAsString();
+                            Log.d("FOO", formattedAddress);
+                        }
+                    }
+                });
+    }
+}
