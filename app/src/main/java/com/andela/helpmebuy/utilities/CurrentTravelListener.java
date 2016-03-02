2f56faca521ff20fde1e763c99805d651e923291
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-02 10:58:13 +0100

    Add CurrentTravelListener interface

---
+++ app/src/main/java/com/andela/helpmebuy/utilities/CurrentTravelListener.java
@@ -0,0 +1,10 @@
+package com.andela.helpmebuy.utilities;
+
+import com.andela.helpmebuy.models.Travel;
+
+/**
+ * Created by andela-jugba on 3/2/16.
+ */
+public interface CurrentTravelListener {
+    void getCurrentTravel(Travel travel);
+}
