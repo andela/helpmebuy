ce33e6d8064b03f19a8a502ac54ee90c610239a1
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-02 11:03:16 +0100

    Refactor CurrentTravelListener interface [Finishes #114287905]

--- app/src/main/java/com/andela/helpmebuy/utilities/CurrentTravelListener.java
+++ app/src/main/java/com/andela/helpmebuy/utilities/CurrentTravelListener.java
@@ -2,9 +2,6 @@ package com.andela.helpmebuy.utilities;
 
 import com.andela.helpmebuy.models.Travel;
 
-/**
- * Created by andela-jugba on 3/2/16.
- */
 public interface CurrentTravelListener {
     void getCurrentTravel(Travel travel);
 }
