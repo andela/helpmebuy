552fe0a9f7cc464896e520fd3f32d1e043c9d27c
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-15 09:25:23 +0100

    Modify CurrentTravelListener class

--- app/src/main/java/com/andela/helpmebuy/utilities/CurrentTravelListener.java
+++ app/src/main/java/com/andela/helpmebuy/utilities/CurrentTravelListener.java
@@ -1,9 +1,7 @@
 package com.andela.helpmebuy.utilities;
 
-import android.widget.Button;
-
 import com.andela.helpmebuy.models.Travel;
 
 public interface CurrentTravelListener {
-    void getCurrentTravel(Button connectButton, Travel travel);
+    void getCurrentTravel(Travel travel);
 }
