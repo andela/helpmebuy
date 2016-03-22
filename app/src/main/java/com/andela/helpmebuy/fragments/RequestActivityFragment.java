56210e62931e6e7190dae07a21fb157f317da01b
andela <kehinde.ogunde@andela.com>
2016-03-22 10:51:27 +0100

    Add countdown method and retry function to RequestActivityFragment

--- app/src/main/java/com/andela/helpmebuy/fragments/RequestActivityFragment.java
+++ app/src/main/java/com/andela/helpmebuy/fragments/RequestActivityFragment.java
@@ -1,12 +1,14 @@
 package com.andela.helpmebuy.fragments;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.andela.helpmebuy.R;
@@ -28,6 +30,8 @@ public class RequestActivityFragment extends Fragment {
     private List<Connection> connections;
     private ConnectionRequestsAdapter requestsAdapter;
     private ProgressWheel progressWheel;
+    private ImageView retry;
+    private CountDownTimer countDownTimer;
 
     public RequestActivityFragment() {
         connections = new ArrayList<>();
@@ -49,6 +53,14 @@ public class RequestActivityFragment extends Fragment {
     }
 
     private void initializeComponents(View view) {
+        retry = (ImageView) view.findViewById(R.id.retry);
+        retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadConnections();
+            }
+        });
+
         requestsAdapter = new ConnectionRequestsAdapter(connections, getContext());
         requestsAdapter.setConnectionRequestListener((ConnectionRequestListener) getActivity());
 
@@ -61,12 +73,14 @@ public class RequestActivityFragment extends Fragment {
     }
 
     private void loadConnections() {
-        progressWheel.spin();
+        countDown();
 
         new FirebaseCollection<>(connectionUrl(), Connection.class)
                 .query(getString(R.string.connectionStatus), 2, new DataCallback<List<Connection>>() {
                     @Override
                     public void onSuccess(List<Connection> data) {
+                        stopTimer();
+
                         if (!data.isEmpty()) {
                             for (Connection connection : data) {
                                 int index = findIndex(connection);
@@ -82,13 +96,13 @@ public class RequestActivityFragment extends Fragment {
                             displayMessage(getString(R.string.no_request_found));
                         }
 
-                        progressWheel.stopSpinning();
+
                     }
 
                     @Override
                     public void onError(String errorMessage) {
                         displayMessage(errorMessage);
-                        progressWheel.stopSpinning();
+                        stopTimer();
                     }
                 });
     }
@@ -109,5 +123,31 @@ public class RequestActivityFragment extends Fragment {
 
     private void displayMessage(String message) {
         Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void countDown() {
+        retry.setVisibility(View.INVISIBLE);
+        progressWheel.spin();
+
+        countDownTimer = new CountDownTimer(15000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                if (progressWheel.isSpinning()) {
+                    progressWheel.stopSpinning();
+                    retry.setVisibility(View.VISIBLE);
+                    displayMessage("No connection request");
+                }
+            }
+        };
+        countDownTimer.start();
+    }
+
+    private void stopTimer() {
+        countDownTimer.cancel();
+        progressWheel.stopSpinning();
     }
 }
