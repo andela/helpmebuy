d9cc490a28101462173efbf766c8c37fea99a35b
Eston Karumbi <eston.karumbi@andela.com>
2016-04-19 14:33:31 +0300

    Implement Contact class

---
+++ app/src/main/java/com/andela/helpmebuy/models/Contact.java
@@ -0,0 +1,17 @@
+package com.andela.helpmebuy.models;
+
+
+public class Contact extends Model {
+
+    private String userId;
+
+    public Contact() {}
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
