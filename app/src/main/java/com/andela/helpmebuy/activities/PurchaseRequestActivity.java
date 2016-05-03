baba5ae7e52cb3c2bf1987e77489bd2bb3742e45
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-03 15:14:27 +0100

    Add fab icon controls to PurchaseRequestActivity class

--- app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
+++ app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
@@ -2,6 +2,9 @@ package com.andela.helpmebuy.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
 
 import com.andela.helpmebuy.R;
 
@@ -11,6 +14,15 @@ public class PurchaseRequestActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_request);
+        String name = getIntent().getExtras().getString("name");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
     }
 
 }
