6cdb91c285516811ff9cd5ca3e1c5183391d039d
andela-Dokocha <daniel.okocha@andela.com>
2016-05-03 10:43:59 +0100

    Add model to hold purchase Items

---
+++ app/src/main/java/com/andela/helpmebuy/models/PurchaseItem.java
@@ -0,0 +1,18 @@
+package com.andela.helpmebuy.models;
+
+/**
+ * Created by andeladev on 03/05/2016.
+ */
+public class PurchaseItem {
+    private String item;
+    private String description;
+    private String cost;
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+}
