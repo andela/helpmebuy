1be2e8e05b7f27e65f637f3afeb1cce1c3a92e3f
andela-Dokocha <daniel.okocha@andela.com>
2016-05-03 10:42:04 +0100

    Add an activity to manage Purchase Requests

---
+++ app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
@@ -0,0 +1,16 @@
+package com.andela.helpmebuy.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.andela.helpmebuy.R;
+
+public class PurchaseRequestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_request);
+    }
+
+}
