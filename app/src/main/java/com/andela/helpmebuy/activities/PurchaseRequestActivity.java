a51ccc7e5bb57e7219e1084512647d172d6676b4
Eston Karumbi <eston.karumbi@andela.com>
2016-05-04 12:29:09 +0300

    Refactor PurchaseRequestActivity

--- app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
+++ app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
@@ -3,12 +3,16 @@ package com.andela.helpmebuy.activities;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.andela.helpmebuy.R;
+import com.andela.helpmebuy.dialogs.PurchaseRequestDialog;
+import com.andela.helpmebuy.models.PurchaseItem;
+import com.andela.helpmebuy.utilities.PurchaseCreateCallback;
 
-public class PurchaseRequestActivity extends Activity {
+public class PurchaseRequestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,23 @@ public class PurchaseRequestActivity extends Activity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                launchAddDialog();
             }
         });
-        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
     }
+
+    private void launchAddDialog() {
+        PurchaseRequestDialog dialog = new PurchaseRequestDialog();
+        dialog.setCallback(purchaseCreateCallback);
+        dialog.show(getSupportFragmentManager(), "addPurchase");
+    }
+
+    private PurchaseCreateCallback purchaseCreateCallback = new PurchaseCreateCallback() {
+        @Override
+        public void onPurchaseCreated(PurchaseItem purchaseItem) {
+            Toast.makeText(PurchaseRequestActivity.this, purchaseItem.getItem(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    };
 
 }
