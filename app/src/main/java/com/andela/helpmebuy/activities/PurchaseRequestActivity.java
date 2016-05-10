2ba22eabef7419c2dfd55ef67c9a7b8067f37006
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-10 09:53:47 +0100

    Create logic for sending purchase request to firebase

--- app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
+++ app/src/main/java/com/andela/helpmebuy/activities/PurchaseRequestActivity.java
@@ -1,24 +1,42 @@
 package com.andela.helpmebuy.activities;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.andela.helpmebuy.R;
+import com.andela.helpmebuy.adapters.PurchaseRequestAdapter;
+import com.andela.helpmebuy.dal.DataCallback;
+import com.andela.helpmebuy.dal.firebase.FirebaseCollection;
 import com.andela.helpmebuy.dialogs.PurchaseRequestDialog;
 import com.andela.helpmebuy.models.PurchaseItem;
+import com.andela.helpmebuy.models.PurchaseRequest;
+import com.andela.helpmebuy.utilities.CurrentUserManager;
+import com.andela.helpmebuy.utilities.ItemDeleteListener;
 import com.andela.helpmebuy.utilities.PurchaseCreateCallback;
+import com.andela.helpmebuy.views.CustomRecyclerView;
 
-public class PurchaseRequestActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class PurchaseRequestActivity extends AppCompatActivity implements ItemDeleteListener {
+    private CustomRecyclerView itemView;
+    private ArrayList<String> items;
+    private PurchaseRequestAdapter purchaseRequestAdapter;
+    private Button sendButton;
+    private String receiverId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_request);
+        initializeComponents();
         String name = getIntent().getExtras().getString("name");
+        receiverId = getIntent().getExtras().getString("userId");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,9 +55,59 @@ public class PurchaseRequestActivity extends AppCompatActivity {
     private PurchaseCreateCallback purchaseCreateCallback = new PurchaseCreateCallback() {
         @Override
         public void onPurchaseCreated(PurchaseItem purchaseItem) {
-            Toast.makeText(PurchaseRequestActivity.this, purchaseItem.getItem(), Toast.LENGTH_LONG)
-                    .show();
+            items.add(purchaseItem.getItem());
+            purchaseRequestAdapter.notifyDataSetChanged();
         }
     };
 
+    private void initializeComponents() {
+        items = new ArrayList<>();
+        itemView = (CustomRecyclerView) findViewById(R.id.purchase_requests_view);
+        purchaseRequestAdapter = new PurchaseRequestAdapter(this, items, this);
+        itemView.setAdapter(purchaseRequestAdapter);
+        itemView.setLayoutManager(new LinearLayoutManager(this));
+        sendButton = (Button) findViewById(R.id.send_request);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createPurchaseRequest();
+            }
+        });
+    }
+
+    @Override
+    public void deleteSelection(View v, int position) {
+        items.remove(position);
+        purchaseRequestAdapter.notifyDataSetChanged();
+    }
+
+    public void createPurchaseRequest() {
+        String id = UUID.randomUUID().toString();
+        String senderId = CurrentUserManager.get(this).getId();
+        String receiverId = this.receiverId;
+        PurchaseRequest purchaseRequest = new PurchaseRequest();
+        purchaseRequest.setId(id);
+        purchaseRequest.setPurchaseList(items);
+        purchaseRequest.setReceiver(receiverId);
+        purchaseRequest.setSender(senderId);
+        createQuery(purchaseRequest,senderId,receiverId);
+    }
+
+    private void createQuery(PurchaseRequest purchaseRequest, String senderId, String receiverId) {
+        sendRequest(purchaseRequest, senderId);
+        sendRequest(purchaseRequest, receiverId);
+
+    }
+
+    private void sendRequest(PurchaseRequest purchaseRequest, String id) {
+        FirebaseCollection<PurchaseRequest> firebaseCollection
+                = new FirebaseCollection<PurchaseRequest>("purchaseRequest/" + id, PurchaseRequest.class);
+        firebaseCollection.save(purchaseRequest, new DataCallback<PurchaseRequest>() {
+            @Override
+            public void onSuccess(PurchaseRequest data) {}
+
+            @Override
+            public void onError(String errorMessage) {}
+        });
+    }
 }
