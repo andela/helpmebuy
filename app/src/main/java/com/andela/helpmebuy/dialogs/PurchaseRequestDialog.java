e858ce78bd027e03d480941bf5c9f6a13b16745d
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-17 11:08:01 +0100

    Update PurchaseRequestDialog with form validation logic

--- app/src/main/java/com/andela/helpmebuy/dialogs/PurchaseRequestDialog.java
+++ app/src/main/java/com/andela/helpmebuy/dialogs/PurchaseRequestDialog.java
@@ -1,9 +1,13 @@
 package com.andela.helpmebuy.dialogs;
 
 import android.app.Dialog;
-import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,6 +31,12 @@ public class PurchaseRequestDialog extends DialogFragment {
 
     private PurchaseCreateCallback callback;
 
+    private TextInputLayout inputLayoutName;
+
+    private TextInputLayout inputLayoutDescription;
+
+    private TextInputLayout inputLayoutCost;
+
     public void setCallback(PurchaseCreateCallback callback) {
         this.callback = callback;
     }
@@ -40,10 +50,16 @@ public class PurchaseRequestDialog extends DialogFragment {
     }
 
     private void initViews() {
+        inputLayoutName = (TextInputLayout) dialog.findViewById(R.id.input_layout_name);
+        inputLayoutDescription = (TextInputLayout) dialog.findViewById(R.id.input_layout_description);
+        inputLayoutCost = (TextInputLayout) dialog.findViewById(R.id.input_layout_cost);
         purchaseName = (TextView) dialog.findViewById(R.id.purchase_name);
         purchaseDescription = (TextView) dialog.findViewById(R.id.purchase_desc);
         purchaseCost = (TextView) dialog.findViewById(R.id.purchase_cost);
         cancelButton = (Button) dialog.findViewById(R.id.btn_cancel_purchase);
+        purchaseName.addTextChangedListener(new MyTextWatcher(purchaseName));
+        purchaseDescription.addTextChangedListener(new MyTextWatcher(purchaseDescription));
+        purchaseCost.addTextChangedListener(new MyTextWatcher(purchaseCost));
         addButton = (Button) dialog.findViewById(R.id.btn_add_purchase);
         assignClickHandlers();
     }
@@ -63,9 +79,7 @@ public class PurchaseRequestDialog extends DialogFragment {
     private View.OnClickListener addListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            PurchaseItem purchaseItem = getPurchaseFromViews();
-            callback.onPurchaseCreated(purchaseItem);
-            dialog.dismiss();
+            submitForm();
         }
     };
 
@@ -76,4 +90,95 @@ public class PurchaseRequestDialog extends DialogFragment {
         item.setItemPrice(purchaseCost.getText().toString());
         return item;
     }
+
+    private boolean validateName() {
+        if (purchaseName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError("Item name cannot be empty");
+            requestFocus(purchaseName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateDescription() {
+        String description = purchaseDescription.getText().toString().trim();
+        if (description.isEmpty()) {
+            inputLayoutDescription.setError("Item description cannot be empty");
+            requestFocus(purchaseDescription);
+            return false;
+        } else {
+            inputLayoutDescription.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+        if (!validateDescription()) {
+            return;
+        }
+
+        if (!validateCost()) {
+            return;
+        }
+        PurchaseItem purchaseItem = getPurchaseFromViews();
+        callback.onPurchaseCreated(purchaseItem);
+        dialog.dismiss();
+    }
+
+
+    private boolean validateCost() {
+        if (purchaseCost.getText().toString().trim().isEmpty()) {
+            inputLayoutCost.setError("Item cost cannot be empty");
+            requestFocus(purchaseCost);
+            return false;
+        } else {
+            inputLayoutCost.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.purchase_name:
+                    validateName();
+                    break;
+                case R.id.purchase_desc:
+                    validateDescription();
+                    break;
+                case R.id.purchase_cost:
+                    validateCost();
+                    break;
+            }
+        }
+    }
 }
+
