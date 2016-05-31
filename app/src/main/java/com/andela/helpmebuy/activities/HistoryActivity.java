c5efcc370561396fcc1a4791599531ce16f5d3b1
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-31 12:20:57 +0100

    Create HistoryActivity class

---
+++ app/src/main/java/com/andela/helpmebuy/activities/HistoryActivity.java
@@ -0,0 +1,43 @@
+package com.andela.helpmebuy.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.andela.helpmebuy.R;
+
+public class HistoryActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.history_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        initializeView();
+    }
+
+    public void initializeView() {
+        String[] history = {"Purchase Request"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.history_item, history);
+        ListView listView = (ListView) findViewById(R.id.history_list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (i == 0) {
+            Intent intent = new Intent(getApplicationContext(), PurchaseHistoryActivity.class);
+            startActivity(intent);
+        }
+    }
+}
